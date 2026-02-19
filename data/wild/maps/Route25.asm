0e4c7f3221c519d93d32d81b55577f905441931b
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-19 19:48:12 +0100

    Update Route25 encounters

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,29 +1,16 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
-	db  9, KAKUNA
-	db 13, PIDGEY
-	db 12, ODDISH
-	db 13, ODDISH
+	db 12, SLOWPOKE 
+	db 12, ODDISH 
+	db 13, BELLSPROUT 
+	db 12, PSYDUCK 
+	db 13, DROWZEE 
 	db 12, ABRA
-	db 14, ODDISH
-	db 10, ABRA
-	db  7, METAPOD
-	db  8, CATERPIE
-ENDC
-IF DEF(_BLUE)
-	db  8, CATERPIE
-	db  9, METAPOD
-	db 13, PIDGEY
-	db 12, BELLSPROUT
-	db 13, BELLSPROUT
-	db 12, ABRA
-	db 14, BELLSPROUT
-	db 10, ABRA
-	db  7, KAKUNA
-	db  8, WEEDLE
-ENDC
+	db 10, BUTTERFREE 
+	db 10, BEEDRILL 
+	db 12, MR_MIME 
+	db 13, MR_MIME 
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
