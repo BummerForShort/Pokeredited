d9a6e703d3e782d69c1e9c76a9f9e492bd4b2c5e
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-19 13:04:08 +0100

    Update Route2 encounters

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,23 +1,23 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
+	db  3, POLIWAG
 	db  3, PIDGEY
-	db  4, PIDGEY
-	db  4, RATTATA
-	db  5, PIDGEY
+	db  3, SPEAROW
+	db  4, PSYDUCK
+	db  4, PSYDUCK
 IF DEF(_RED)
-	db  3, WEEDLE
-	db  2, RATTATA
-	db  5, RATTATA
-	db  4, WEEDLE
-	db  5, WEEDLE
+	db  3, MANKEY
+	db  4, MANKEY
+	db  3, DODUO
+	db  4, DODUO
+	db  4, EEVEE
 ENDC
 IF DEF(_BLUE)
-	db  3, CATERPIE
-	db  2, RATTATA
-	db  5, RATTATA
-	db  4, CATERPIE
-	db  5, CATERPIE
+	db  3, MANKEY
+	db  4, MANKEY
+	db  3, DODUO
+	db  4, DODUO
+	db  4, EEVEE
 ENDC
 	end_grass_wildmons
 
