a6d228f5d2e49fb6cb16ae5eb63f731e1f42b276
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 15:48:01 +0100

    Update PokemonTower6F encounters

--- data/wild/maps/PokemonTower6F.asm
+++ data/wild/maps/PokemonTower6F.asm
@@ -1,14 +1,14 @@
 PokemonTower6FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
 	db 20, GASTLY
-	db 19, GASTLY
-	db 26, HAUNTER
+	db 21, GASTLY
+	db 22, ZUBAT
+	db 23, PARAS
+	db 21, GASTLY
+	db 25, HAUNTER
+	db 22, GOLBAT
+	db 20, CUBONE
 	db 22, CUBONE
-	db 24, CUBONE
 	db 28, HAUNTER
 	end_grass_wildmons
 
