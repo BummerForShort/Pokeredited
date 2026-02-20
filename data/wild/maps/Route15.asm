1cb4d136fb72f0bb8181cf7039e33393823bd231
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 10:42:34 +0100

    Update Route15 encounters

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,27 +1,15 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
-IF DEF(_RED)
-	db 24, ODDISH
+	db 26, RATICATE
+	db 26, MEOWTH
+	db 26, PIDGEOTTO
 	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, ODDISH
+	db 27, VENONAT
 	db 28, VENONAT
-	db 26, ODDISH
-	db 30, GLOOM
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, BELLSPROUT
-	db 28, VENONAT
-	db 26, BELLSPROUT
-	db 30, WEEPINBELL
-ENDC
-	db 28, PIDGEOTTO
-	db 30, PIDGEOTTO
+	db 28, PERSIAN
+	db 29, TAUROS
+	db 28, TAUROS
+	db 30, TAUROS
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
