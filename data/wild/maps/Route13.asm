5871d353b395ebefb94c2a8f5b7a2b1eeac8de8b
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 10:35:41 +0100

    Update Route13 encounters

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -1,29 +1,15 @@
 Route13WildMons:
 	def_grass_wildmons 20 ; encounter rate
-IF DEF(_RED)
-	db 24, ODDISH
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
-	db 25, DITTO
-	db 28, GLOOM
-	db 30, GLOOM
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 25, PIDGEY
-	db 27, PIDGEY
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 25, DITTO
-	db 28, WEEPINBELL
-	db 30, WEEPINBELL
-ENDC
+	db 26, MANKEY
+	db 26, MACHOP 
+	db 27, FEAROW
+	db 26, FARFETCHD
+	db 28, MACHOKE
+	db 26, PRIMEAPE 
+	db 26, PRIMEAPE 
+	db 27, HITMONLEE
+	db 28, FARFETCHD 
+	db 28, HITMONLEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
