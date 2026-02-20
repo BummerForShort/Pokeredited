7a77c0a8947c60ec4a24f74c1a455fe165193a7e
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 13:22:31 +0100

    Update Route17 encounters

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -1,15 +1,15 @@
 Route17WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 19, SPEAROW
-	db 22, DODUO
-	db 19, PIDGEOTTO
-	db 20, FARFETCHD 
-	db 22, FEAROW
-	db 20, EKANS 
+	db 20, KOFFING
+	db 22, GRIMER
+	db 21, EKANS
+	db 20, PONYTA 
+	db 20, FEAROW
+	db 20, DODUO 
 	db 22, DODUO
 	db 22, ARBOK
-	db 23, RATICATE
-	db 25, RATICATE
+	db 23, TAUROS
+	db 25, TAUROS
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
