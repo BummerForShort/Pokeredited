3916241b9dfbfb8bde7550f291d88deae3ab5253
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 15:38:52 +0100

    Update PokemonMansion3F encounters

--- data/wild/maps/PokemonMansion3F.asm
+++ data/wild/maps/PokemonMansion3F.asm
@@ -1,29 +1,15 @@
 PokemonMansion3FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
-	db 31, KOFFING
-	db 33, GROWLITHE
-	db 35, KOFFING
-	db 32, PONYTA
+    db 32, KOFFING
+	db 32, GRIMER
 	db 34, PONYTA
-	db 40, WEEZING
-	db 34, GRIMER
-	db 38, WEEZING
-	db 36, PONYTA
-	db 42, MUK
-ENDC
-IF DEF(_BLUE)
-	db 31, GRIMER
-	db 33, VULPIX
-	db 35, GRIMER
-	db 32, PONYTA
-	db 34, MAGMAR
-	db 40, MUK
-	db 34, KOFFING
-	db 38, MUK
-	db 36, PONYTA
-	db 42, WEEZING
-ENDC
+	db 30, VULPIX
+	db 34, GROWLITHE
+	db 33, MAGMAR
+	db 35, MAGMAR
+	db 28, PONYTA
+	db 37, WEEZING
+	db 39, MUK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
