c5135edb9b682d62aa2387c32d4cc9443a7b6c22
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 15:47:22 +0100

    Update PokemonTower5F encounters

--- data/wild/maps/PokemonTower5F.asm
+++ data/wild/maps/PokemonTower5F.asm
@@ -2,14 +2,14 @@ PokemonTower5FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 20, GASTLY
 	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 19, GASTLY
-	db 18, GASTLY
+	db 22, ZUBAT
+	db 23, PARAS
+	db 21, GASTLY
 	db 25, HAUNTER
+	db 22, GOLBAT
 	db 20, CUBONE
 	db 22, CUBONE
-	db 24, GASTLY
+	db 25, HAUNTER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
