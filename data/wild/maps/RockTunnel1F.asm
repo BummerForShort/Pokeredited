67468dfc0f56ef26ea9572ce6dd36934aeac7ce8
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 13:41:19 +0100

    Update RockTunnel1F encounters

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,15 +1,15 @@
 RockTunnel1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
 	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 15, ZUBAT
 	db 17, MACHOP
-	db 13, ONIX
-	db 15, ONIX
+	db 17, GEODUDE
+	db 17, CUBONE
+	db 17, SANDSHREW 
+	db 18, ONIX
+	db 17, ONIX
+	db 17, KANGASKHAN 
+	db 18, KANGASKHAN 
+	db 19, KANGASKHAN 
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
