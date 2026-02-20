6cdbab5256a9e5a92a131ed1280fabe028bb4b51
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-20 13:38:20 +0100

    Update SeaRoutes encounters

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -3,14 +3,14 @@ SeaRoutesWildMons:
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACOOL
-	db 40, TENTACOOL
+	db 28, TENTACOOL
+	db 30, SEEL
+	db 30, PSYDUCK 
+	db 34, TENTACRUEL 
+	db 33, SEAKING
+	db 33, GOLDUCK 
+	db 34, DEWGONG
+	db 35, DEWGONG
+	db 35, GYARADOS
+	db 33, TENTACRUEL
 	end_water_wildmons
