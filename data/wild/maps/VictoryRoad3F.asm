8e918958af4d699c09d934cf467aeb2eef773613
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-22 18:08:31 +0100

    Update VictoryRoad3F encounters

--- data/wild/maps/VictoryRoad3F.asm
+++ data/wild/maps/VictoryRoad3F.asm
@@ -1,15 +1,15 @@
 VictoryRoad3FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 24, MACHOP
-	db 26, GEODUDE
-	db 22, ZUBAT
-	db 42, ONIX
-	db 40, VENOMOTH
-	db 45, ONIX
-	db 43, GRAVELER
-	db 41, GOLBAT
-	db 42, MACHOKE
-	db 45, MACHOKE
+	db 38, MACHOKE
+	db 37, GRAVELER 
+	db 38, GOLBAT 
+	db 38, ONIX
+	db 39, VENOMOTH 
+	db 42, HAUNTER 
+	db 42, RHYDON
+	db 42, RHYDON 
+	db 42, MAROWAK
+	db 43, MAROWAK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
