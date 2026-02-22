151d5320654f2cd3265d653cee6ddabba69240f4
BummerForShort <48069176+BummerForShort@users.noreply.github.com>
2026-02-22 18:00:15 +0100

    Update SeafoamIslandsB4F encounters

--- data/wild/maps/SeafoamIslandsB4F.asm
+++ data/wild/maps/SeafoamIslandsB4F.asm
@@ -1,28 +1,15 @@
 SeafoamIslandsB4FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
-	db 31, HORSEA
-	db 31, SHELLDER
-	db 33, HORSEA
-	db 33, SHELLDER
-	db 29, SLOWPOKE
-	db 31, SEEL
-	db 31, SLOWPOKE
-	db 29, SEEL
-	db 39, SLOWBRO
-ENDC
-IF DEF(_BLUE)
-	db 31, KRABBY
-	db 31, STARYU
-	db 33, KRABBY
-	db 33, STARYU
-	db 29, PSYDUCK
-	db 31, SEEL
-	db 31, PSYDUCK
-	db 29, SEEL
-	db 39, GOLDUCK
-ENDC
-	db 32, GOLBAT
+    db 30, SEEL
+	db 30, HORSEA 
+	db 30, STARYU
+	db 30, SHELLDER
+	db 28, POLIWHIRL
+	db 30, SLOWPOKE
+	db 29, GOLBAT
+	db 28, VAPOREON
+	db 35, GOLDUCK 
+	db 38, POLIWRATH
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
